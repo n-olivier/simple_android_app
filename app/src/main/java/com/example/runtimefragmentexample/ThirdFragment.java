5504178ff51773e6c6cbd3fde0846bd533c4ebbf
n-olivier <onshimiyimana17@alustudent.com>
2020-02-13 14:08:32 +0200

    fixed a small bug

--- app/src/main/java/com/example/runtimefragmentexample/ThirdFragment.java
+++ app/src/main/java/com/example/runtimefragmentexample/ThirdFragment.java
@@ -8,12 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ThirdFragment extends Fragment {
+    private Button button;
 
 
     public ThirdFragment() {
@@ -25,6 +27,7 @@ public class ThirdFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_third, container, false);
     }
 
